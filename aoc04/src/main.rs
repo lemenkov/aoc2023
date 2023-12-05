69afa4b2b1da5c0623102fc1e597b12f3b7455ce
Peter Lemenkov <lemenkov@gmail.com>
2023-12-06 00:18:55 +0100

    AoC 04.2
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- aoc04/src/main.rs
+++ aoc04/src/main.rs
@@ -6,6 +6,9 @@ fn main() {
     // File hosts must exist in current path before this produces output
     if let Ok(lines) = read_lines("./input") {
         let mut sum = 0u32;
+
+        let mut cards: Vec<(u32, u32)> = vec![];
+
         for raw_line in lines {
             if let Ok(line) = raw_line {
                 let v: Vec<&str> = line.split(&[':', '|'][..]).filter(|&x| !x.is_empty()).collect();
@@ -24,9 +27,13 @@ fn main() {
                 if count > 0 {
                     sum += base.pow(count-1);
                 }
+
+                cards.push((count, 1));
             }
         }
         println!("SUM: {}", sum);
+        println!("CARDS TOTAL: {}" , count_cards(cards));
+
     }
 }
 
@@ -36,4 +43,22 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
     where P: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
+}
+
+fn count_cards(mut cards: Vec<(u32, u32)>) -> u32 {
+    let lenght = cards.len();
+    let mut total = 0u32;
+
+    let mut i: usize = 0;
+    while i < lenght {
+        let mut j: usize = 0;
+        while j < cards[i].0.try_into().unwrap() {
+            cards[i + j + 1].1 += cards[i].1;
+            j += 1;
+        }
+        total += cards[i].1;
+        i += 1;
+    }
+
+    return total;
 }
